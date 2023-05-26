84a3b0b20c85ce79d364be771f10f1ec8b1c5a4f
arpitktyr <arpitktyr@gmail.com>
2023-05-26 12:30:50 +0530

    loading and some small issue fixed

--- src/redux/catSlice.js
+++ src/redux/catSlice.js
@@ -5,17 +5,20 @@ export const catSlice = createSlice({
   initialState: {
     data: [],
     isLoading: false,
+    haveError: "",
   },
   reducers: {
     getCatsFetch: (state) => {
       state.isLoading = true;
+      state.haveError = "";
     },
     getCatsSuccess: (state, action) => {
       state.data = action.payload;
       state.isLoading = false;
     },
-    getCatsFailure: (state) => {
+    getCatsFailure: (state, action) => {
       state.isLoading = false;
+      state.haveError = action.payload;
     },
   },
 });
--- src/redux/saga.js
+++ src/redux/saga.js
@@ -1,10 +1,14 @@
 import { call, put, takeEvery } from "redux-saga/effects";
-import { getCatsSuccess } from "./catSlice";
+import { getCatsFailure, getCatsSuccess } from "./catSlice";
 
 function* workerCatFetch() {
-  const data = yield call(() => fetch("https://api.thecatapi.com/v1/breeds"));
-  const cats = yield data.json();
-  yield put(getCatsSuccess(cats.slice(0, 20)));
+  try {
+    const data = yield call(() => fetch("https://api.thecatapi.com/v1/breeds"));
+    const cats = yield data.json();
+    yield put(getCatsSuccess(cats.slice(0, 30)));
+  } catch (e) {
+    yield put(getCatsFailure("Something went wrong !!"));
+  }
 }
 
 function* catSaga() {
