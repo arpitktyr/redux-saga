0024c88076216228a6d090b4b7dbaf6feadf1260
arpitktyr <arpitktyr@gmail.com>
2023-05-26 15:49:00 +0530

    Footer added

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p> This website is designed and made by Arpit Katiyar </p>
+    </div>
+  );
+};
+
+export default Footer;
